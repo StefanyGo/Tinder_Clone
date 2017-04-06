8e200dfc9caaf5d5bcb37d02076b969e64f22567
StefanyGo <gunawang@purdue.edu>
2017-04-06 18:50:10 -0400

    first commit

---
+++ Tinder_Stef/DraggableImageView.swift
@@ -0,0 +1,21 @@
+//
+//  DraggableImageView.swift
+//  Tinder_Stef
+//
+//  Created by Stefany Felicia on 4/4/17.
+//  Copyright © 2017 Make School. All rights reserved.
+//
+
+import UIKit
+
+class DraggableImageView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
